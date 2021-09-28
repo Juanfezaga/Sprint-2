3b9e9815378b0ba84000ee43354b137a185bf2be
Juanfezaga <juanfezaga@gmail.com>
2021-09-28 10:48:19 -0500

    Initial Setup

---
+++ form.js
@@ -0,0 +1,16 @@
+//Comienzo del Archivo
+
+function checkNumDocumento(valor, tipo){
+  return false
+}
+
+
+function checkContraseña(valor){
+  return false
+}
+
+function checkCorreo(valor){
+  return false
+}
+
+module.exports = {checkContraseña, checkCorreo, checkNumDocumento}
